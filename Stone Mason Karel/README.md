7eefebee5d9d8931e7dc2d689b99553bed7b06f9
ronaldo sanchez <ronaldo2017unheval@gmail.com>
2025-06-21 23:47:49 -0500

    update

--- Stone Mason Karel/README.md
+++ Stone Mason Karel/README.md
@@ -1,10 +1,22 @@
 # STONE MASON KAREL
-Your task is to help Karel fill the world with beepers, regardless of its size.
 
-For example, consider Karel's starting 5x5 world. When complete it should look like this:
-
-
-Note that Karel's final position does matter. Karel should be in the top-right corner facing right. You can assume that Karel always starts in the bottom-left corner, facing right. Your code needs to work on more than just a 5x5 world. For example if you run your solution on the 3x4 world
+Karel has been hired to build the columns in the Temple of Artemis in Efes. In particular, there are a set of arches where the stones (represented by beepers, of course) are missing from the columns supporting the arches, as follows:
 
 
-The result should still be a world filled with beepers:
+
+When Karel is done, the missing columns should be replaced by beepers, so the final picture would look like this:
+
+
+
+Karel may count on the following facts about the world, listed below:
+
+    - Karel starts at bottom left corner, facing right (aka east).
+    - The columns are exactly four squares apart, on the 1st, 5th, 9th, and 13th columns.
+    - Karel can assume that columns are always five units high.
+
+
+Your program will be much easier to write and easier to read, if you use for loops. There are multiple opportunities to use for loops in this problem!
+
+In addition to for loops, you should be practicing defining functions. For example, it would make sense to have a build_column function.
+
+Good luck and ask questions!
--- Stone Mason Karel/main.py
+++ Stone Mason Karel/main.py
@@ -1,48 +1,43 @@
 from karel.stanfordkarel import *
 
 """
-Karel should fill the whole world with beepers.
+File: main.py
+--------------------
+When you finish writing this file, Karel should have repaired 
+each of the columns in the temple
 """
 
 def main():
-    """
-    You should write your code to make Karel do its task in
-    this function. Make sure to delete the 'pass' line before
-    starting to write your own code. You should also delete this
-    comment and replace it with a better, more descriptive one.
-    """
-    while left_is_clear():
-        fill_row()
-        step_column()
-    move_to_corner()
-
-def fill_row():
-    move_to_corner()
-    turn_around()
-    return_row()
-
-def move_to_corner():
-    put_beeper()
-    while front_is_clear():
-        move()
-        put_beeper()
-
-def turn_around():
-    turn_left()
-    turn_left()
-
-def return_row():
-    while front_is_clear():
-        move()
-
-def step_column():
-    turn_right()
-    move()
-    turn_right()
     
-def turn_right():
     for i in range(3):
-        turn_left()
-# There is no need to edit code beyond this point
+        up_column()
+        down_column()
+        move_next_column()
+    up_column()
+    down_column()
+    """
+    build the columns in the temple of artemis
+    """
+def up_column():
+    turn_left()
+    build_column()
+
+def down_column():
+    turn_left()
+    turn_left()
+    for i in range (4):
+        move()
+    turn_left()
+
+def move_next_column():
+    for i in range (4):
+        move()
+
+def build_column():
+    while front_is_clear():
+        put_beeper()
+        move()
+    put_beeper()
+
 if __name__ == '__main__':
     main()
