e7df918297ff36362772dbaff606fc2c3533995f
ronaldo sanchez <ronaldo2017unheval@gmail.com>
2025-06-23 00:30:05 -0500

    Random Numbers

---
+++ Random Numbers/main.py
@@ -0,0 +1,16 @@
+import random
+
+N_NUMBERS = 10
+MIN_VALUE = 1
+MAX_VALUE = 100
+
+def main():
+    """
+    You should write your code here. Make sure to delete 
+    the 'pass' line before starting to write your own code.
+    """
+    for i in range(10):
+        value=random.randint(1,100)
+        print(value)
+if __name__ == '__main__':
+    main()
